fb098c3d81ba4ed90bdbacf69f5143f387186182
bendix <bendix@benoto.net>
2026-04-23 20:15:50 +0200

    Fixed Problem with Telescope -> Wrong position for function

--- lua/benoto/plugins/telescope.lua
+++ lua/benoto/plugins/telescope.lua
@@ -1,11 +1,4 @@
 -- Telescope
-
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-
 return {
     'nvim-telescope/telescope.nvim', version = '*',
     dependencies = {
@@ -17,6 +10,11 @@ return {
     },
     config = function() 
         local telescope = require("telescope")
+        local builtin = require("telescope.builtin")
+        vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+        vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
         telescope.setup({
             pickers = {
                 find_files = {
