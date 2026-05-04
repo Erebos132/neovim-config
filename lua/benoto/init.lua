89f88c73bb8c76356e4af1f4656225a182476c0d
Bendix <bendix@benoto.net>
2026-05-04 20:14:53 +0200

    Removed Folding line on the left (irritating)

--- lua/benoto/init.lua
+++ lua/benoto/init.lua
@@ -23,7 +23,7 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 
 -- Folding
-vim.o.foldcolumn = "1"
+vim.o.foldcolumn = "0"
 vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
